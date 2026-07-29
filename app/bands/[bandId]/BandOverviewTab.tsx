f1d6428f2d9ff2b5c9ceb520d10131cd0fb68b36
zanedr <zanedanielrussell@gmail.com>
2026-07-29 15:58:24 -0600

    update View event menu

--- app/bands/[bandId]/BandOverviewTab.tsx
+++ app/bands/[bandId]/BandOverviewTab.tsx
@@ -141,28 +141,26 @@ export function BandOverviewTab({
             >
               Edit event
             </ActionMenuItem>
-            {show.setlistId && (
-              <>
-                <ActionMenuItem
-                  onClick={() =>
-                    router.push(`/bands/${bandId}/setlists/${show.setlistId}`)
-                  }
-                >
-                  View setlist
-                </ActionMenuItem>
-                <ActionMenuItem
-                  onClick={() =>
-                    router.push(`/bands/${bandId}/setlists/${show.setlistId}/edit`)
-                  }
-                >
-                  Edit setlist
-                </ActionMenuItem>
-              </>
-            )}
             {canDownload &&
               downloadTarget &&
               (offlineRec ? (
                 <>
+                  <ActionMenuItem
+                    onClick={() =>
+                      router.push(`/bands/${bandId}/setlists/${show.setlistId}`)
+                    }
+                  >
+                    View setlist
+                  </ActionMenuItem>
+                  <ActionMenuItem
+                    onClick={() =>
+                      router.push(
+                        `/bands/${bandId}/setlists/${show.setlistId}/edit`,
+                      )
+                    }
+                  >
+                    Edit setlist
+                  </ActionMenuItem>
                   <ActionMenuItem
                     onClick={() =>
                       router.push(
